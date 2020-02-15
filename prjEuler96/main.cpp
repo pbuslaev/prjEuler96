8ada00bb1903935fac862d9b8f857b45578081eb
pbuslaev <pbuslaev@gmail.com>
2020-02-16 01:55:46 +0300

    Initial Commit

---
+++ prjEuler96/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  prjEuler96
+//
+//  Created by Павел Буслаев on 16/02/2020.
+//  Copyright © 2020 Павел Буслаев. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
